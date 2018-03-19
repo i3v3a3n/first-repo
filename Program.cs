66272dd258348b464afde062686c2171dda3725e
ivan_danov <ivan_danov@yahoo.com>
2018-03-19 11:58:16 +0200

    add debit card numbers

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var First = int.Parse(Console.ReadLine()); // read the input 
+            var Second = int.Parse(Console.ReadLine());
+            var Third = int.Parse(Console.ReadLine());
+            var Forth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0:d4} {1:d4} {2:d4} {3:d4}", First, Second, Third, Forth); // out the result 
+        }
+    }
+}
